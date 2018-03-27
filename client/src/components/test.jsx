9f8078d021169d7376ab77c2370851fb5e79ee36
Jimmy Sanders-Cannon <jimmysancan@yahoo.com>
2018-03-27 15:23:55 -0400

    env complete

--- client/src/components/test.jsx
+++ client/src/components/test.jsx
@@ -1,23 +1,30 @@
 import React from 'react'
+import '../css/test.css'
 
 class Test extends React.Component {
   constructor() {
     super()
     this.state = {
-      names: []
+      authors: []
     }
   }
   componentDidMount() {
     fetch('/api/fakeData')
     .then(res => res.json())
-    .then(names => this.setState({names}, () =>console.log('fakeData route test', this.state.names)))
+    .then(authors => this.setState({authors}, () => console.log('authors fetched', authors)))
   }
+  
   render() {
     return (
       <div>
         <p>I looked into the windows of her SOUL ! and I said: Biiiiiiiiitch </p>
         <div>
-          <p>Project By:</p>
+          <p>Project By: </p>
+          <ul>
+            {this.state.authors.map(author => 
+              <li key={author.id}> {author.first} {author.last} - id: {author.id}  </li>
+            )}
+          </ul>  
         </div>
       </div>
     )
